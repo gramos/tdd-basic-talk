670b4b612bd0aaaef759421732d3eda7a400fb80
Gaston Ramos <ramos.gaston@gmail.com>
2014-07-02 10:24:51 -0300

    Rot can change the encoder.

--- ejercicios.rb
+++ ejercicios.rb
@@ -1,25 +1,23 @@
 require 'minitest/autorun'
 
 class Rot
+  @@plain_chars_set = "abcdefghijklmnopqrstuvwxyz"
 
-  @@plain_chars_set   = "abcdefghijklmnopqrstuvwxyz"
-  @@replace_chars_set = "nopqrstuvwxyzabcdefghijklm"
-
-  def initialize(message)
-    @message = message
+  def initialize(message, replace_chars_set = "nopqrstuvwxyzabcdefghijklm")
+    @replace_chars_set = replace_chars_set
+    @message           = message
   end
 
   def encode
-    @message.tr @@plain_chars_set, @@replace_chars_set
+    @message.tr @@plain_chars_set, @replace_chars_set
   end
 
   def decode
-    @message.tr @@replace_chars_set, @@plain_chars_set
+    @message.tr @replace_chars_set, @@plain_chars_set
   end
 end
 
 class TestRot < MiniTest::Unit::TestCase
-
   def test_that_encode_a_simple_string
     rot = Rot.new("simple")
     assert_equal "fvzcyr", rot.encode
@@ -33,5 +31,10 @@ class TestRot < MiniTest::Unit::TestCase
   def test_that_decode_strings
     rot = Rot.new("lrg nabgure")
     assert_equal "yet another", rot.decode
+  end
+
+  def test_that_can_change_the_encoder
+    rot = Rot.new("simple", "xbcdxfghxjklmnxpqrstxvwxyz")
+    assert_equal "sxmplx", rot.encode
   end
 end
