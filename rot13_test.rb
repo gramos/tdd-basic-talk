9a6008fe80c660f6e7687aa76ebda5c0a77552e1
Gaston Ramos <ramos.gaston@gmail.com>
2014-07-04 09:03:58 -0300

    Add exercise resolved in the talk.

--- rot13_test.rb
+++ rot13_test.rb
@@ -1,7 +1,24 @@
+# Este lo resolvimos en la charla de la meetup de rubylit
+# el 3/7/2014
+
 require 'minitest/autorun'
 
-# "abcdefghijklmnopqrstuvwxyz"
-# "nopqrstuvwxyzabcdefghijklm"
+def rot13(c)
+  c.tr "abcdefghijklmnopqrstuvwxyz", "nopqrstuvwxyzabcdefghijklm" 
+end
 
 class TestRot < MiniTest::Unit::TestCase
+
+  def test_encrypt_a
+    assert_equal "n", rot13("a")
+  end
+
+  def test_encrypt_b
+    assert_equal "o", rot13("b")
+  end
+
+  def test_encrypt_n
+    assert_equal "a", rot13("n")
+  end
+
 end
